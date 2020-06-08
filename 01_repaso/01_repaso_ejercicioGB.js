a236c6538b0e1cbe7f77d94b6b520b19454d8d20
Jofre <aejofre@gmail.com>
2020-06-08 17:45:59 -0500

    git ignore

--- 01_repaso/01_repaso_ejercicioGB.js
+++ 01_repaso/01_repaso_ejercicioGB.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const { Banda } = require('./banda');
+// const { Banda } = require('./banda');
 
 request.get('https://ghibliapi.herokuapp.com/films', (err, res, body) => {
     if (res.statusCode === 200) {
@@ -13,14 +13,42 @@ request.get('https://ghibliapi.herokuapp.com/films', (err, res, body) => {
 
 });
 
-const pomiseGhibli = new Promise((resolve, reject) => {
-    request.get('https://ghibliapi.herokuapp.com/films'), (err, res, body) => {
-        if (res.statusCode === 200) resolve(JSON.parse(body));
-        reject(res.statusCode);
+// const pomiseGhibli = new Promise((resolve, reject) => {
+//     request.get('https://ghibliapi.herokuapp.com/films'), (err, res, body) => {
+//         if (res.statusCode === 200) resolve(JSON.parse(body));
+//         reject(res.statusCode);
 
-    };
+//     };
+// });
+
+
+
+// pomiseGhibli
+//     .then(films => films.map(films => console.log(films.name)))
+//     .catch(err => console.log(err))
+
+
+// const pomiseLast = new Promise((resolve, reject) => {
+//     request.get(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=birtney+spearss&api_key=7e4c33016dda25dc8ad05f453fe614bc&format=json`), (err, res, body) => {
+//         if (res.statusCode === 200) resolve(JSON.parse(body));
+//         reject(res.statusCode);
+
+//     };
+// });
+
+// pomiseLast
+//     .then(films => films.map(films => console.log(films.name)))
+//     .catch(err => console.log(err))
+
+request.get(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Kylie+Minogue&api_key=7e4c33016dda25dc8ad05f453fe614bc&format=json`, (err, res, body) => {
+
+
+    if (res.statusCode === 200) {
+        const artist = JSON.parse(body);
+        artist.map(artist => {
+            console.log(artist.name);
+        })
+        } else {
+            console.log('Error en la petición');
+        }
 });
-
-pomiseGhibli
-.then(films => films.map(films => console.log(films.name)))
-.catch(err => console.log(err))
